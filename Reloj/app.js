382ec1e325cfc8c6d6cc8135fdfca1f661ac329b
yarkin-05 <uoftmetas@gmail.com>
2023-11-06 08:40:14 -0600

    Finished Javascript

--- Reloj/app.js
+++ Reloj/app.js
@@ -1,27 +1,19 @@
 function time(){
   const d = new Date; //new date object
-  let hours = d.getHours();
-  let minutes  = d.getMinutes();
-  let seconds =  d.getSeconds();
 
-  //console.log(time);
-  console.log(hours);
-  console.log(minutes);
-  console.log(seconds);
+  
   let msj = document.createElement('p'); //msj en msj
   let time = document.createElement('p');
 
-  if( hours >= 19 || hours <= 4){ msj.textContent = "Buenas Noches" ;}
-  else if(hours >= 5 && hours <= 12) {msj.textContent = "Buenos dias";}
-  else if(hours >= 12 && hours <= 18) {msj.textContent="Buenas tardes";}
+  if( d.getHours() >= 19 || d.getHours() <= 4) msj.textContent = "Buenas Noches" ;
+  else if(d.getHours() >= 5 && d.getHours() <= 12) msj.textContent = "Buenos dias";
+  else if(d.getHours() >= 12 && d.getHours() <= 18) msj.textContent="Buenas tardes";
   time.textContent = hours+":"+minutes+":"+seconds;
-
+  $('#reloj').html(time);
+  $('#msg').html(msj)
 
   let clock = document.querySelector('#reloj');
   let show_message = document.querySelector('#msg');
-
-  clock.appendChild(time);
-  show_message.appendChild(msj);
 
 }
 
