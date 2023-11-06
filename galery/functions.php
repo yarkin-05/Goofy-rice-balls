4863f5928e5654d1a6d8ea543a201f65e7e6bc66
yarkin-05 <uoftmetas@gmail.com>
2023-11-06 11:55:11 -0600

    Added my JS file and jQuery

--- galery/functions.php
+++ galery/functions.php
@@ -44,6 +44,8 @@ EOT;
 
 function template_footer(){
   echo <<<EOT
+  <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
   <script src="app.js"></script>
   </body>
 </html>
