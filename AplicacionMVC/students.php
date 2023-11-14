c2078eba1596f9e1049b5e7c22e9a86cfe5c479a
yarkin-05 <uoftmetas@gmail.com>
2023-11-14 08:21:54 -0600

    preparing for merge

--- AplicacionMVC/students.php
+++ AplicacionMVC/students.php
@@ -1,11 +1,15 @@
 <?php
 //Archivo de vista
 
-  include 'connect.php'; //function that connects to the database
+include 'connect.php'; //function that connects to the database
+if (!isset($firstaccess)) { 
   $pdo = pdo_connect_mysql(); //the conectin the function returns
 
-  $stmt = $pdo -> query('SELECT * FROM students ORDER BY name DESC'); // gets the array to all the students in the students table and orders them alphabetically
+  $stmt = $pdo -> query('SELECT * FROM students ORDER BY id DESC'); // gets the array to all the students in the students table and orders them alphabetically
   $students = $stmt->fetchAll(PDO::FETCH_ASSOC);//returns the array from the query
+  }  
+$firstaccess = true;
+
 ?>
 
 <?=template_header('Students Table')?>
--- AplicacionMVC/studentsController.php
+++ AplicacionMVC/studentsController.php
@@ -1,57 +1,21 @@
-
 <?php
-//Archivo que maneja las peticiones, dependiendo los parametros seleccionados, se van a ejecutar el método solicitado e incluye la vista en caso de ser necesario. 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include 'studentModel.php'; // funciones de todo
 
-include 'studentModel.php'; //funciones de todo
-include 'connect.php';
-$operation = $_POST['action']; //type of action
+$operation = isset($_GET['action']) ? $_GET['action'] : null;
+$id = isset($_GET['id']) ? $_GET['id'] : null;
 
-if(isset($operation) && $operation === 'register') 
-{
-  //si quiere añadir un registro
-}
-else{
-  $student = new Students(); // Instance of class
-  if(isset($operation) && $_POST['id'] === '') {
-  //si tiene id
-  switch($operation) {
-    case 'getAll':
-        $student->getAll();
-        echo json_encode($student);
-      break;
+//echo 'action: ' .$_GET['action'].'\n\n'; // Debugging line
 
-    case 'Register':
-      $stmt = $student->store(); // Get the prepared pdo statement
-    
-    // Parameters
-    $name = $_POST['name'];
-    $username = $_POST['username'];
-    $lastname = $_POST['lastname'];
-    $birthday = $_POST['birthdate'];
-    
-    // Bind parameters
-    $stmt->bindParam(1, $username);
-    $stmt->bindParam(2, $name);
-    $stmt->bindParam(3, $lastname);
-    $stmt->bindParam(4, $birthday);
-
-    // Execute the statement
-    $stmt->execute();
-    break;
-  }
-  }else{
-    //no tiene id
-    switch($operation) {
-      case 'details':
-        break;
-      case 'update':
-        break;
-      case 'delete':
-        break;
-    }
-
-    }
+$student = new Students(); // Instantiate the Students class
+if ($operation === 'show') {
+    //header('Content-Type: application/json');
+    //var_dump($result);
+    echo json_encode($student->getAll());
+} else {
+    echo 'Unknown operation ';
 }
 
-
-
+?>
